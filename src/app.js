ef68c13a33b17de4aa7c856da26e70cfaf24eb1e
Borgarelli <borgarellitavares@outlook.com>
2022-10-29 14:51:02 -0300

    add dataBinding

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const MyNameApp = { //Para retornar um nome predefinido e uma idade
     data(){
         return{
-            name: "Kauã",
+            name: "",
             age: 19,
             input_name:""//Linkando o v-model com o data, semrpe que um usuário digitar um nome no formulário, sera armazenado aqui
 
@@ -9,7 +9,12 @@ const MyNameApp = { //Para retornar um nome predefinido e uma idade
     },
     methods: { //Onde armazena as funções da aplicação
         submitForm(e) {
-            e.preventDefault();
+            e.preventDefault(); //Um evento para evitar que o formulário seja enviado para o servidor
+
+            console.log(this.input_name); //Evento para retornar no console o nome inserido pelo usuário
+
+            this.name = this.input_name; //Aqui o Vue irá transformar o input de nome, no valor que o input_name recebe através do v-model utilizado dentro do input, exemplo: o name recebe um valor nulo de inicio, e o valor que input_name receber será atribuido a name pelo vue
+
         }
 
     }
