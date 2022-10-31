94ec6324fee40565b704616e2290307d5754db87
Kauã Borgarelli <79945984+Borgarelli@users.noreply.github.com>
2022-10-31 16:57:28 -0300

    add methods

--- 2_projeto_vue/src/components/Info.vue
+++ 2_projeto_vue/src/components/Info.vue
@@ -9,7 +9,10 @@
             <li>Vue</li>
             <li>JavaScript</li>
         </ul>
-        <p v-show="mostrar_email">Mande uma mensagem para esse email {{email}}</p>
+        <div>
+            <button @click="showEmail">{{texto_button}}</button><!--O @click está prrporcionado um evento para o button, sendo referenciado pelo metodo passado dentro do export default como objeto methods-->
+        </div>
+        <p v-show="mostrar_email">Mande uma mensagem para esse email {{email}}</p><!--Cria como se fosse uma requisição estilo v-if e else, porém o v-show apenas mostra caso seja uma condição true, caso seja false ela não retorna nada-->
         <p>Para acessar meu portfólio <a v-bind:href="meu_link" target="_blank">basta clicar aqui</a></p><!--v-bind é o responsavel por conseguir mudar o link para o parametro definido no data, conseguindo fazer conexão dos 2-->
         <Picture />
     </div>
@@ -25,9 +28,20 @@ import Picture from './Picture.vue';
         data() {
             return {              
                 esta_trabalhando: false, //Cria o parametro de que caso seja true ele irá retornar a mensagem, e caso seja falso não retorna mensagem nenhuma
-                mostrar_email: true,
-                email:'borgarelli@outlook.com',
-                meu_link: 'https://google.com'
+                mostrar_email: false, //Alterado para false para mostrar o funcionamento do metodo criado para mostrar o paragrafo com o email
+                email: 'borgarelli@outlook.com',
+                meu_link: 'https://google.com',
+                texto_button: 'Mostrar e-mail'
+            }
+        },
+        methods: {
+            showEmail() {
+                this.mostrar_email = !this.mostrar_email //Cria o metodo para quando o usuário clicar no botão, ele mostre a mensagem que contém o email, e quando ele clicar denovo ele esconda o paragrafo com o email
+                if(!this.mostrar_email) { //Caso mostrar email esteja definido como false ele irá trocar o texto do botão para Mostrar emial, caso esteja como verdadeiro ele mostra a mensagem de esconder, sincronizando com os eventos do botão
+                    this.texto_button = 'Mostrar e-mail'
+                } else {
+                    this.texto_button = 'Esconder e-mail'
+                }
             }
         }
     }
--- 2_projeto_vue/src/components/LifeCycle.vue
+++ 2_projeto_vue/src/components/LifeCycle.vue
@@ -14,12 +14,20 @@
             setTimeout(() => {  //Aqui define o tempo em que ocorrerá a substituição desses dados após o carregamento da página, Exemplo: A página irá carregar por padrão com o valor 'Ainda não sei', porém 1 segundo depois ocorrerá essa troca de valores pelo valor inserido dentro timeOut
                 this.nome = 'Kauã'
             }, 1000);
+
+            this.lifeCycle() //Instancia o metodo, mostrando que foi executado corretamente no console do navegador
         },
         mounted() {//Evento que está acima da hierarquia do created que acaba "passando por cima" e realocando outro dado em cima dele
             setTimeout(() => {
                 this.nome = 'Borgarelli'
             }, 2000); 
             
+            this.lifeCycle()
+        },
+        methods: {
+            lifeCycle() {
+                console.log("Executado") //Metódo criado apenas para mostrar que methods funcionava dentro dos triggers
+            }
         }
     }
 </script>
