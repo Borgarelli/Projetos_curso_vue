85576e55ce4a7459ff55756e6dbebc04744f059e
Kauã Borgarelli <79945984+Borgarelli@users.noreply.github.com>
2022-11-02 16:26:19 -0300

    multiples events

---
+++ 2_projeto_vue/src/components/MultiplosEventos.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <button @click="primeiro(txt1, $event), segundo(txt2, $event), terceiro(txt3, $event)">Ativar Multiplos Eventos</button> <!--Criou um evento que assim que o usuário clicar no botão ativa o metódo "Primeiro", que envia um valor para o data, e o metodo "Primeiro" pega esse valor e atribui uma mensagem a esse valor e assim imprime uma mensagem na tela-->
+        <p>{{multiplos_eventos}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'MultiplosEventos',
+        data() {
+            return {
+                multiplos_eventos: "",
+                txt1: 'Primeiro evento',
+                txt2: 'Segundo evento',
+                
+            }
+        },
+        methods: {
+            primeiro(txt, e) {
+                this.multiplos_eventos = txt;
+            },
+            segundo(txt, e) {
+                setTimeout(() => {
+                this.multiplos_eventos = txt}, 500); //Cria o segundo evento, que assim que o usuário clicar no botão e ativar o primeiro evento, depois de alguns segundo irá ativar o segundo, mudando a mensagem para a mensagem armazenada no data
+        },
+            terceiro(txt, e) {
+                setTimeout(() => {
+                    this.multiplos_eventos = ''}, 700); //terceiro evento para limpar a mensagem
+            }
+    }
+}
+</script>
+
+<style scoped>
+    div{
+        margin:100px;
+    }
+</style>
--- 2_projeto_vue/src/components/PrimeiroComponente.vue
+++ 2_projeto_vue/src/components/PrimeiroComponente.vue
@@ -2,12 +2,17 @@
     <div> <!--Por questões de semantica o Vue só pode fazer uma requisição do data por vez, então o certo seria colocar todas as requisições dentro de uma div-->
         <h1>Olá Vue</h1>
         <p>Meu nome é {{nome}} e trabalho como {{profissão}}</p> 
+        <MultiplosEventos />
     </div>
 </template>
 
 <script>
+import MultiplosEventos from './MultiplosEventos.vue';
     export default {
         name: 'PrimeiroComponente',
+        components: {
+            MultiplosEventos
+        },
         data() {
             return { // Instancia os dados que iram ser retornados quando requisitados dentro do template ou mais conhecidos como chaves
                 nome: "Kauã", 
