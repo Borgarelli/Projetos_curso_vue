dc30c6f8a5aa68ff1ead210eeb128fbef31c5d21
Kauã Borgarelli <79945984+Borgarelli@users.noreply.github.com>
2022-11-02 14:32:31 -0300

    add v-for

--- 2_projeto_vue/src/components/Info.vue
+++ 2_projeto_vue/src/components/Info.vue
@@ -2,12 +2,19 @@
     <div>
         <p v-if="esta_trabalhando">No momento eu estou trabalhando</p> <!--Aqui ele introduz a condicional utilizando um componente proprio do Vue, o v-if, caso o parametro esteja definido como true, a mensagem ira retornar na tela, caso esteja definido como falso, ele não ira retornar nada-->
         <p v-else>Estou em busca de novas oportunidades</p> <!--Mensagem que irá retornar quando a condição do v-if não for cumprida, detalhe: v-else sempre tem que estar abaixo do v-if para funcionar-->
-        <p>Utilizo as seguintes tecnologias: </p>
+        <p>Utilizo as seguintes tecnologias para front-end: </p>
+        <ul> 
+            <li v-for="(technology, index) in frontend_technologies" v-bind:key="index">
+                {{technology}}
+            </li> <!--Implementando o v-for, primeiro dando a condição do technology como se fosse uma string de referencia, e assim utilizando-o para imprimir o array (lista) definida dentro do data -->   
+            <!--O v-for por padrão ele irá repetir o elemento pai de acordo com o número de elementos definidos dentro do array. Ele funciona, porém gera um erro de semantica que é necessário como se fosse inserir um id proprio para repetição, para consertar esse erro é necessário criar um v-bind:key dentro da tag para armazenar esse id, e para gerar esse id é bem simples, basta abrir um () onde ele define a condição, no caso desse exemplo é o technology e colocar um index, no caso vai ficar 
+v-for ="(technology, index) in frontend_technology" v-bind:key="index"-->
+        </ul>
+        <p>Utilizo as seguintes tecnologias para back-end:</p>
         <ul>
-            <li>Html</li>
-            <li>Css</li>
-            <li>Vue</li>
-            <li>JavaScript</li>
+            <li v-for="technology in backend_technologies" :key="technology.id"> <!--Aqui não precisa do index, pois como ela é um array de objeto ela ja vem definida com uma chave propria para cada repetição, então é só puxar através do technology.id-->
+            {{technology.language}} <!--Aqui para retornar apenas a linguagem defindia no array na tela-->
+            </li>
         </ul>
         <div>
             <button @click="showEmail">{{texto_button}}</button> <!--O @click está proporcionado um evento para o button, sendo referenciado pelo método passado dentro do export default como objeto no methods-->
@@ -31,7 +38,13 @@ import Picture from './Picture.vue';
                 mostrar_email: false, //Alterado para false para mostrar o funcionamento do metodo criado para mostrar o paragrafo com o email
                 email: 'borgarelli@outlook.com',
                 meu_link: 'https://google.com',
-                texto_button: 'Mostrar e-mail'
+                texto_button: 'Mostrar e-mail',
+                frontend_technologies: ['Html', 'CSS', 'Vue'], //Criando um array de formato simples
+                backend_technologies: [ //Trazendo mais um Array porém em formato de objetos, para caso seja uma lista com mais complexidade
+                    {id: 1, language: 'Python'},
+                    {id: 2, language: 'Java'},
+                    {id: 3, language: 'JavaScript' }
+                ]
             }
         },
         methods: {
