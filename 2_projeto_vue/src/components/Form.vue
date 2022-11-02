5d78b67f596d54ebbe8b27cba3555e5d70fd1bf1
Kauã Borgarelli <79945984+Borgarelli@users.noreply.github.com>
2022-11-02 15:13:28 -0300

    add @submit and methods

--- 2_projeto_vue/src/components/Form.vue
+++ 2_projeto_vue/src/components/Form.vue
@@ -1,19 +1,22 @@
 <template>
     <div>
         <h2>Meu formulário</h2>
-        <form>
+        <form action="" @submit="enviarFormulario($event)"> <!--Atribuindo um evento ao formulário de quando ele enviar algum valor dentro dos inputs, ele recarregar a página por padrão, porém com esse $event atual, definido dentro do methods ele impede de cumprir esse evento, não tendo um reload na página, mas ainda recebendo um valor quando é feio o submit, porém só pode ser visto atráves do console-->
             <div> <!--Só para quebrar linha-->
-                <InputText />
+                <!--<InputText /> retirado parra a aula de eventos-->
+                <input type="text" v-model="name"> <!--Esse input está conectado diretamente com o data, que vai armazenar os valores ali-->
             </div>
 
             <div>
-                <InputText /> <!--Aqui ele está fazendo duas requisições, porém o Vue enxerga como valores diferentes -->
+                <!--<InputText /> Aqui ele está fazendo duas requisições, porém o Vue enxerga como valores diferentes -->
+                <input type="email" v-model="email">
             </div>
 
             <div>
                 <Submit />
             </div>
         </form>
+        <p>Conteudo</p> <!--Só para espaçamento-->
     </div>
 </template>
 
@@ -26,6 +29,35 @@ import Submit from './form/Submit.vue';
         components: {
             InputText,
             Submit
+        },
+        data() {
+            return {
+                name: '',
+                email:''
+            }
+        },
+        methods: {
+            enviarFormulario(e) {
+
+                e.preventDefault(); //Evento que previne que o site tenha um reload quando enviado os valores inseridos no formulário
+
+                const name = this.name; //Aqui vai pegar os dados armazenados dentro do data e definilos como os valores inseridos
+                const email = this.email;
+
+                console.log("Formulário Enviado");
+                console.log("O nome é: " + name); //Aqui vai imprimilos dentro do console
+                console.log("O email é: " + email);
+
+                // Ajax em um projeto real, aqui seria feito uma requisição em Ajax para tratar os dados inseridos nos formulários
+
+                // Inserir no banco os dados / E aqui seria feito a requisição para armazenar esses dados dentro de um banco
+            }
         }
     }
 </script>
+
+<style scoped>
+    p{
+        margin: 50px;
+    }
+</style>
